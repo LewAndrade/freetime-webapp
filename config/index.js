659f916dd170fd144ee34e4613796f8e7d9a701c
Lew Andrade <andrade.lew@gmail.com>
2021-07-05 15:29:27 -0300

    Add mock server

--- config/index.js
+++ config/index.js
@@ -1,3 +1,7 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://lew.netlify.com";
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://lewfreetime.netlify.com";
+
+export const server = "http://localhost:3000";
